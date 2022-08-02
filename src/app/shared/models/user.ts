811c58bfe6ca60e1a71718bd783cd1b956e28cad
bkarkusashvili <beqa.qarqusashvili.20@gmail.com>
2022-08-02 15:14:28 +0400

    fix user component

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,8 +1,10 @@
+import { SafeHtml } from '@angular/platform-browser';
+
 export class User {
     id: string;
     crated_time: number;
     created: string;
     karma: number;
     avg: number;
-    about: string;
+    about: string | SafeHtml;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,37 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Subscription } from 'rxjs/Subscription';
+import { switchMap, tap } from 'rxjs/operators';
 
 import { HackerNewsAPIService } from '../shared/services/hackernews-api.service';
 import { User } from '../shared/models/user';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit {
-  sub: Subscription;
-  user: User;
-  errorMessage = '';
+export class UserComponent implements OnInit, OnDestroy {
+    sub: Subscription;
+    userId: string;
+    user: User;
+    errorMessage = '';
 
-  constructor(
-    private _hackerNewsAPIService: HackerNewsAPIService,
-    private route: ActivatedRoute,
-    private _location: Location
-  ) {}
+    constructor(
+        private _hackerNewsAPIService: HackerNewsAPIService,
+        private route: ActivatedRoute,
+        private _location: Location,
+        private domSanitizer: DomSanitizer
+    ) {}
 
-  ngOnInit() {
-    this.sub = this.route.params.subscribe(params => {
-      let userID = params['id'];
-      this._hackerNewsAPIService.fetchUser(userID).subscribe(data => {
-        this.user = data;
-      }, error => this.errorMessage = 'Could not load user ' + userID + '.');
-    });
-  }
+    ngOnInit() {
+        this.sub = this.route.paramMap
+            .pipe(
+                tap((paramMap) => (this.userId = paramMap.get('id'))),
+                switchMap(() => this._hackerNewsAPIService.fetchUser(this.userId))
+            )
+            .subscribe(
+                (data) =>
+                    (this.user = {
+                        ...data,
+                        about: this.domSanitizer.bypassSecurityTrustHtml(data.about as string),
+                    }),
+                () => (this.errorMessage = 'Could not load user ' + this.userId + '.')
+            );
+    }
 
-  goBack() {
-    this._location.back();
-  }
+    goBack() {
+        this._location.back();
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
 }
